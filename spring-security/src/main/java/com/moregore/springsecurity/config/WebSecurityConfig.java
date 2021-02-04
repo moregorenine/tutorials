4bdfbafc1dd32b4bd13a115edddebd1eba59f856
moregorenine <moregorenine@gmail.com>
2021-02-05 02:01:12 +0900

    init overwrite WebSecurityConfigurerAdapter

---
+++ spring-security/src/main/java/com/moregore/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.moregore.springsecurity.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+        http
+                .formLogin();
+    }
+
+}
