4235dab33d0eaef8ddb6cfdfc66714c1535076dd
moregorenine <moregorenine@gmail.com>
2021-02-05 02:32:37 +0900

    feature formLogin()

--- spring-security/src/main/java/com/moregore/springsecurity/config/WebSecurityConfig.java
+++ spring-security/src/main/java/com/moregore/springsecurity/config/WebSecurityConfig.java
@@ -5,6 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +28,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         http
-                .formLogin();
+                .formLogin()
+//                .loginPage("/loginPage")   // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/") // 로그인 성공 후 이동 페이지
+                .failureUrl("/login")   // 로그인 실패 후 이동 페이지
+                .usernameParameter("username")  // 아이디 파라미터명 설정
+                .passwordParameter("password")  // 패스워드 파라미터명 설정
+                .loginProcessingUrl("/login")   // 로그인 Form Action Url
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        log.debug("authentication {}", authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })  // 로그인 성공 후 핸들러
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        log.debug("exception {}", exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })  // 로그인 실패 후 핸들러
+                .permitAll();
+        ;
     }
 
 }
--- spring-security/src/main/java/com/moregore/springsecurity/controller/SecurityController.java
+++ spring-security/src/main/java/com/moregore/springsecurity/controller/SecurityController.java
@@ -10,4 +10,9 @@ public class SecurityController {
     public String index() {
         return "home";
     }
+
+    @GetMapping(value = "/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
 }
