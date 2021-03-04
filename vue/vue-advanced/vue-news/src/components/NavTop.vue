e486f28c526abb7588ac214438e495e3a82667ed
moregorenine <moregorenine@gmail.com>
2021-03-05 01:03:00 +0900

    update nav-top css

--- vue/vue-advanced/vue-news/src/components/NavTop.vue
+++ vue/vue-advanced/vue-news/src/components/NavTop.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-      <router-link to="/news">News</router-link>
-      <router-link to="/ask">Ask</router-link>
-      <router-link to="/jobs">Jobs</router-link>
+  <div class="navtop">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style scoped>
+.navtop {
+  color: white;
+  background: #42b883;
+  display: flex;
+  padding: 8px;
+}
 
-<style></style>
+.navtop .router-link-exact-active {
+    color: #35495e;
+}
+
+.navtop a {
+    color: white;
+}
+</style>
