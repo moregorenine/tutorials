cd271d9e68abe894cb6b64070a329528ae73413a
moregorenine <moregorenine@gmail.com>
2021-03-08 23:27:07 +0900

    update news list styling

--- vue/vue-advanced/vue-news/src/views/NewsView.vue
+++ vue/vue-advanced/vue-news/src/views/NewsView.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <p v-for="item in getNews" v-bind:key="item.id">
+    <ul class="news-list">
+      <li v-for="item in getNews" v-bind:key="item.id" class="post">
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link v-bind:to="`/user/${item.user}`" class="link-text">
+              {{ item.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+    <!-- <p v-for="item in getNews" v-bind:key="item.id">
       <a v-bind:href="item.url">{{ item.title }}</a>
       <small>
         {{ item.time_ago }} by
         <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
       </small>
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -46,4 +66,33 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
+}
+</style>
