a006bb4214036cd9f2b0219ad018afde87ba8d3a
moregorenine <moregorenine@gmail.com>
2021-03-08 23:49:57 +0900

    update JobsView list styling

--- vue/vue-advanced/vue-news/src/views/JobsView.vue
+++ vue/vue-advanced/vue-news/src/views/JobsView.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <p v-for="item in getJobs" v-bind:key="item.id">
-      <a v-bind:href="item.url">{{ item.title }}</a>
-      <small> {{ item.time_ago }} by {{ item.domain }}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="item in getJobs" v-bind:key="item.id" class="post">
+        <div class="points">
+          {{ item.points || 0 }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">{{ item.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ item.time_ago }} by
+            <a v-bind:href="item.url">{{ item.domain }}</a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,4 +32,33 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+
+.news-title {
+  margin: 0;
+}
+
+.link-text {
+  color: #828282;
+}
+</style>
