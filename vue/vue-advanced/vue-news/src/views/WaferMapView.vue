c2c89b9aa3b139f8ed520e83cd0b10ce68e7e472
moregorenine <moregorenine@gmail.com>
2021-03-18 01:58:54 +0900

    feature plugins

--- vue/vue-advanced/vue-news/src/views/WaferMapView.vue
+++ vue/vue-advanced/vue-news/src/views/WaferMapView.vue
@@ -1,15 +1,10 @@
 <template>
   <div>
-    <svg width="500" height="500" id="target1"></svg>
-    <svg width="500" height="500" id="target2"></svg>
-    <svg width="500" height="500" id="target3"></svg>
-    <svg width="500" height="500" id="target4"></svg>
-    <svg width="500" height="500" id="target5"></svg>
-    <wafer-map tagetid="target1"></wafer-map>
-    <wafer-map tagetid="target2"></wafer-map>
-    <wafer-map tagetid="target3"></wafer-map>
-    <wafer-map tagetid="target4"></wafer-map>
-    <wafer-map tagetid="target5"></wafer-map>
+    <wafer-map></wafer-map>
+    <wafer-map></wafer-map>
+    <wafer-map></wafer-map>
+    <wafer-map></wafer-map>
+    <wafer-map></wafer-map>
   </div>
 </template>
 
