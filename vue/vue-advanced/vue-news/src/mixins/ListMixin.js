de5d00fad6a4007591b793f38ee536fda04596ce
moregorenine <moregorenine@gmail.com>
2021-03-17 23:40:19 +0900

    feature mixin and fixed vuex store actions return chain

---
+++ vue/vue-advanced/vue-news/src/mixins/ListMixin.js
@@ -0,0 +1,19 @@
+import bus from '../utils/bus.js';
+
+export default {
+  // 재사용할 컴포넌트 옵션
+  created() {
+    const start = new Date();
+    bus.$emit('start:spinner');
+    setTimeout(() => {
+      this.$store
+        .dispatch('FETCH_LIST', this.$route.name)
+        .then(() => {
+          const end = new Date();
+          bus.$emit('end:spinner');
+          alert(`${end - start} ms`);
+        })
+        .catch((error) => console.log(error));
+    }, 3000);
+  },
+};
