a38ace986f44b8c1d6b5a4d2a953560aedbd68bb
moregorenine <moregorenine@gmail.com>
2021-03-21 22:45:54 +0900

    feature cof

---
+++ vue/vuesion/src/app/cof/mutations.spec.ts
@@ -0,0 +1,15 @@
+import { CofMutations } from './mutations';
+import { CofDefaultState, ICofState } from './state';
+
+describe('CofMutations', () => {
+  let testState: ICofState;
+
+  beforeEach(() => {
+    testState = CofDefaultState();
+  });
+
+  test('Please write the tests for the mutations here', () => {
+    // here is an example: https://github.com/vuesion/vuesion/blob/master/src/app/counter/mutations.spec.ts
+    expect(true).toBeFalsy();
+  });
+});
---
+++ vue/vuesion/src/app/cof/mutations.ts
@@ -0,0 +1,17 @@
+import { ICofState } from './state';
+
+export interface ICofMutations {
+  /**
+   * put your mutations names, parameters and return values here, for example:
+   * MY_MUTATION(context: ActionContext<ICofState, IState>, param: any): void;
+   */
+}
+
+export const CofMutations: ICofMutations = {
+  /**
+   * here comes the implementation of your mutations, for example:
+   * MY_MUTATION: (state, param) {
+   *   return state.myAttribute = param;
+   * }
+   */
+};
